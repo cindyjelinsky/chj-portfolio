193057d919f85a2ade220cb2c21d3e7884612025
c.helena <cindyjelinsky@yahoo.com.br>
2026-03-19 15:40:17 -0300

    url fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfoliov2/dev-portfolio/',
+  base: 'https://github.com/cindyjelinsky/chj-portfolio',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
